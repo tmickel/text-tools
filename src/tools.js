d4ad69e91f23c5fa37d32f6e73c26329716cec9e
Big Fun Cloud TODO <tim@tmickel.com>
2021-05-02 13:10:30 +0000

    Automated from Big Fun Cloud: M  src/index.html M  src/tools.js

--- src/tools.js
+++ src/tools.js
@@ -9,7 +9,8 @@ const textareas = document.getElementsByTagName("textarea");
 for (let textarea of textareas) {
     textarea.addEventListener("focusout", (e) => {
         const str = e.target.value;
-        tools[textarea.id](str)
+        const result = tools[textarea.id](str);
+        document.getElementById(`${textarea.id}-result`).innerHTML = result;
     });
 }
 
