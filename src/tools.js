22018f4e1384df93e38216efb8a8edcbe4a05ad7
Big Fun Cloud TODO <tim@tmickel.com>
2021-05-02 13:13:17 +0000

    Automated from Big Fun Cloud: M  src/tools.js

--- src/tools.js
+++ src/tools.js
@@ -1,6 +1,6 @@
 class Tools {
     shuffleCharacters(str) {
-        const chars = s.split('');
+        const chars = str.split('');
         const shuffleArray = (array) => {
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
